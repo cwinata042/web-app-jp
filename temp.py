e03703d12f6a74919b552d2b6df4c354422e3e3a
cwinata042 <77115831+cwinata042@users.noreply.github.com>
2023-10-23 14:01:33 -0400

    adds all table visuals
    TO-DO:
    - Handle downloading a dataframe into .csv file
    - Allow selecting file path

--- temp.py
+++ temp.py
@@ -1,18 +1,38 @@
 import pandas as pd
 import pyodide
 import io
-import js
+from js import document
 
-table1 = Element('table1')
+table1 = document.getElementById('table1')
+table2 = document.getElementById('table2')
+table3 = document.getElementById('table3')
 
 async def get_file(e):
     files = e.target.files.to_py()
     for file in files:
       file_content = await file.text()
       df = pd.read_csv(io.StringIO(file_content))
-      table1.write(df.head())
-      # table1.write(df.to_html(escape=False, classes="table"))
+
+      # Displays original table
+      table1.innerHTML = df.to_html(classes="table", index=False)
+
+      # Displays modified tables
+      temp_modify1(df)
+      table2.innerHTML = df.to_html(classes="table", index=False)
+      temp_modify2(df)
+      table3.innerHTML = df.to_html(classes="table", index=False)
+
+def temp_modify1(df):
+   df.columns = ['C', 'B', 'A']
+   df[['C', 'B', 'A']] = df[['A', 'B', 'C']]
+
+def temp_modify2(df):
+   df.columns = ['C', 'B', 'A']
+   df[['C', 'B', 'A']] = df[['A', 'C', 'B']]
 
 get_file_proxy = pyodide.ffi.create_proxy(get_file)
 
 js.document.getElementById("csv").addEventListener("change", get_file_proxy)
+# TO-DO: Handle downloading a dataframe into .csv file
+#       Allow selecting file path
+# js.document.getElementById('download').addEventListener("onClick", downloadFile)
